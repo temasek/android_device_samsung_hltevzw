592a4858aa687f017a9246fcbbdb637693fc9112
Janson Kang <temasek71@gmail.com>
2015-03-23 07:34:17 +0800

    hltevzw: Enable OTA update

--- device.mk
+++ device.mk
@@ -14,6 +14,15 @@
 # limitations under the License.
 #
 
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.ota.romname=temasek-hltevzw \
+    ro.ota.version=$(shell date +%F | sed s@-@@g) \
+    ro.ota.manifest=http://temasek.rajasthanautoworks.in/ota/hltevzw.xml
+
+PRODUCT_PACKAGES += \
+    OTAUpdates \
+    libbypass
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get non-open-source specific aspects
